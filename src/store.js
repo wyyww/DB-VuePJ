57388fc5dcb50bc76f189d4131df0b884737e773
JMEPJ\PC <1485165003@qq.com>
2017-12-18 11:29:10 +0800

    引入状态管理vuex，了解状态管理机制，后面有需要再去填充状态管理

---
+++ src/store.js
@@ -0,0 +1,27 @@
+
+/*
+*Vuex
+* https://vuex.vuejs.org/zh-cn/
+*/
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(vuex);
+
+const store = new Vuex.Store({
+    state:{
+
+    },
+    mutations:{
+
+    },
+    getters:{
+
+    },
+    actions:{
+
+    },
+})
+
+export default store;

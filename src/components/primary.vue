2ce2d7b35ea7c2fd7e80c4b9a4c641510b2b093e
JMEPJ\PC <1485165003@qq.com>
2017-12-19 16:02:54 +0800

    引入vue-resource实现前后端数据交互，并测试一组接口数据是否匹配；引入vue-router用于后面单页面路由配置

--- src/components/primary.vue
+++ src/components/primary.vue
@@ -3,12 +3,35 @@
       <div class="section">
         电影
       </div>
-      <div class="slider">
+      <div class="slider" @click="resourceGet()">
         电影口碑
       </div>
   </div>
 </template>
 <script>
+
+export default{
+  name:"primary",
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    resourceGet(){
+      var that = this;
+      that.$http.jsonp('https://api.douban.com/v2/movie/in_theaters')
+       .then(response =>{
+         console.log(response);
+       },response=>{
+         console.log("error" + response);
+       })
+
+    }
+  }
+}
+
+
 </script>
 
 <style lang="less" scoped>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import 'babel-polyfill';
 
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueResource from 'vue-resource'
+
 import App from './App';
 import store from './store';
 
+Vue.use(Vuex);
+Vue.use(VueResource);
 Vue.config.devtools = true;
 
 new Vue({

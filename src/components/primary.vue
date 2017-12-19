0fea4de43fc274e3824e650b48aed9207b4675bd
JMEPJ\PC <1485165003@qq.com>
2017-12-19 17:06:54 +0800

    做了一部分的豆瓣电影，正在上映部分，还需要接着完善主要内容

--- src/components/primary.vue
+++ src/components/primary.vue
@@ -1,37 +1,57 @@
 <template>
   <div class="content">
-      <div class="section">
-        电影
+      <div class="article">
+          <div class="screening" @click="resourceGet()">
+            <div class="screening-hd">
+              <div class="ui-slide-control">
+                <span class="prev-btn">
+                  <a href="">往前翻</a>
+                </span>
+                <span class="next-btn">
+                  <a href="">往后翻</a>
+                </span>
+              </div>
+              <div class="slide-tip">
+                <span class="ui-slide-tip">5</span>
+                /
+                <span class="ui-slide-max">6</span>
+              </div>
+              <h2>
+                正在热映
+                <span><a href="#" >全部正在热映>></a></span>
+                <span><a href="#" >即将上映>></a></span>
+              </h2>
+            </div>
+            <div class="screening-bd">
+              美丽的图片
+            </div>
+          </div>
       </div>
-      <div class="slider" @click="resourceGet()">
+      <div class="slider">
         电影口碑
       </div>
   </div>
 </template>
 <script>
-
-export default{
-  name:"primary",
-  data(){
-    return{
-
-    }
+export default {
+  name: "primary",
+  data() {
+    return {};
   },
-  methods:{
-    resourceGet(){
+  methods: {
+    resourceGet() {
       var that = this;
-      that.$http.jsonp('https://api.douban.com/v2/movie/in_theaters')
-       .then(response =>{
-         console.log(response);
-       },response=>{
-         console.log("error" + response);
-       })
-
+      that.$http.jsonp("https://api.douban.com/v2/movie/in_theaters").then(
+        response => {
+          console.log(response);
+        },
+        response => {
+          console.log("error" + response);
+        }
+      );
     }
   }
-}
-
-
+};
 </script>
 
 <style lang="less" scoped>
@@ -40,16 +60,36 @@ export default{
   padding: 35px 15%;
   background-color: #fff;
 
-  .section {
+  .article {
     background-color: #808080;
     width: 60%;
     float: left;
     margin-right: 8%;
-    border:1px solid #fff;
-   
+
+    .screening {
+      margin-bottom: 36px;
+
+      h2 {
+        font-size: 16px;
+        color: #111;
+        padding-bottom: 10px;
+        border-bottom: 1px solid #eaeaea;
+        margin-bottom: 18px;
+      }
+
+      .ui-slide-control {
+        float: right;
+        margin: 5px 0 0 15px;
+      }
+      .slide-tip {
+        float: right;
+        line-height: 30px;
+        color: #666;
+      }
+    }
   }
 
-  .slider{
+  .slider {
     background-color: #f00;
     overflow: hidden;
   }

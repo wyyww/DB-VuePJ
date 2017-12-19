42bb9d96070da0687add1dd273c9797c99b74b4c
JMEPJ\PC <1485165003@qq.com>
2017-12-19 11:33:51 +0800

    主体部分通过块级作用域实现两栏布局，尝试在vue中进行接口调用、调试

--- src/components/primary.vue
+++ src/components/primary.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="content">
-      sfksdk
+      <div class="section">
+        电影
+      </div>
+      <div class="slider">
+        电影口碑
+      </div>
   </div>
 </template>
 <script>
-    
 </script>
 
 <style lang="less" scoped>
+.content {
+  position: relative;
+  padding: 35px 15%;
+  background-color: #fff;
 
+  .section {
+    background-color: #808080;
+    width: 60%;
+    float: left;
+    margin-right: 8%;
+    border:1px solid #fff;
+   
+  }
+
+  .slider{
+    background-color: #f00;
+    overflow: hidden;
+  }
+}
 </style>
